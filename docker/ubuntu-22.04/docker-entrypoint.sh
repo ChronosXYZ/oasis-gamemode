69af6f784c01928f10fc6fde55eee8e83209343f
ChronosXYZ <chronosxyz000@gmail.com>
2024-01-16 03:26:59 +0300

    Skip CMake configuration on build if SKIP_CMAKE env var is set

--- docker/ubuntu-22.04/docker-entrypoint.sh
+++ docker/ubuntu-22.04/docker-entrypoint.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
 [ -z $CONFIG ] && config=Release || config="$CONFIG"
 
-cmake \
-    -S . \
-    -B build \
-    -G Ninja \
-    -DCMAKE_C_FLAGS=-m32 \
-    -DCMAKE_CXX_FLAGS=-m32 \
-    -DCMAKE_BUILD_TYPE=$config \
-&&
+if [ "$SKIP_CMAKE" != "true" ]; then 
+    cmake \
+        -S . \
+        -B build \
+        -G Ninja \
+        -DCMAKE_C_FLAGS=-m32 \
+        -DCMAKE_CXX_FLAGS=-m32 \
+        -DCMAKE_BUILD_TYPE=$config
+fi
+
 cmake \
     --build build \
     --config $config \
